7372d796f637d37c0a60c080d1b8efadcc16e68d
Kavyasree Daggupati <kavyasreedaggupati@Kavyasrees-Air.attlocal.net>
2025-02-14 07:14:14 -0600

    test

--- src/main/java/com/java/learning/LearningApplication.java
+++ src/main/java/com/java/learning/LearningApplication.java
@@ -8,6 +8,7 @@ public class LearningApplication {
 
 	public static void main(String[] args) {
 		System.out.println("Hello");
+		System.out.println("Hello");
 		SpringApplication.run(LearningApplication.class, args);
 	}
 
